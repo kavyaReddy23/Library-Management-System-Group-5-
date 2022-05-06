3853b842b62b51cc2958c83224f832fc79f29221
Sanskar Gaur <sanskargaur24@gmail.com>
2022-05-06 15:36:43 +0530

    updated book,employee,transaction dao

--- Library Management/src/lib/management/persistence/EmployeeDao.java
+++ Library Management/src/lib/management/persistence/EmployeeDao.java
@@ -1,5 +1,6 @@
 package lib.management.persistence;
 
 public interface EmployeeDao {
-
+	void issueBook(int empId);
+	void returnBook(int empid);
 }
--- Library Management/src/lib/management/persistence/TransactionDaoImpl.java
+++ Library Management/src/lib/management/persistence/TransactionDaoImpl.java
@@ -1,5 +1,54 @@
 package lib.management.persistence;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class TransactionDaoImpl implements TransactionDao {
+
+	private Connection connection;
+
+	public TransactionDaoImpl() {
+		super();
+		this.connection = DataConnection.getConnection();
+	}
+
+	@Override
+	public void issueBook(int empId, int bookId) {
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+		Date issueDate = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.add(Calendar.DAY_OF_YEAR, 7);
+		Date expectedReturnDate = calendar.getTime();
+		Statement statement = null;
+		try {
+			statement = connection.createStatement();
+			String insertInTransaction = "insert into transaction (bookId,empId,issueDate,expectedReturnDate,actualReturnDate,isReturned) values("
+					+ bookId + "," + empId + "," + formatter.format(issueDate) + ","
+					+ formatter.format(expectedReturnDate) + "," + "null" + "," + "false)";
+			statement.executeUpdate(insertInTransaction);
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void returnBook(int empid, int bookId) {
+		Statement statement = null;
+
+		try {
+			statement = connection.createStatement();
+			String updatingTransactionTable = "update transaction set isReturned = True";
+			statement.executeUpdate(updatingTransactionTable);
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+	}
 
 }
