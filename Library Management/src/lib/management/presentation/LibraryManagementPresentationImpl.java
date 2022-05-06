f813d0997505eff0a22f7231760e71ad1bd12d72
Roshni Chhetri <roshnichhetri1510@gmail.com>
2022-05-06 20:52:00 +0530

    Added switch cases on perform choice by employeeid function in presentation layer

--- Library Management/src/lib/management/presentation/LibraryManagementPresentationImpl.java
+++ Library Management/src/lib/management/presentation/LibraryManagementPresentationImpl.java
@@ -26,7 +26,15 @@ public class LibraryManagementPresentationImpl implements LibraryManagementPrese
 	}
 
 	public void showMenuForEmployee() {
-	    System.out.println("Press 1 : Search Book By Type:");
+	    System.out.println("Press 1: Search Employee Details:");
+	    System.out.println("Press 2: Search Book By Type:");
+	    System.out.println("Press 3: Search Book By Name:");
+	    System.out.println("Press 4: Search Book By Keyword:");
+	    System.out.println("Press 5: Search Book By Author Name:");
+	    System.out.println("Press 6: Search My Details:");   
+	    System.out.println("Press 7: Issue Book:");
+	    System.out.println("Press 8: Return Book:");
+	    
 		int choice =scanner.nextInt();
 		performChoiceByEmployeeID(choice);
 	}	
@@ -35,14 +43,63 @@ public class LibraryManagementPresentationImpl implements LibraryManagementPrese
 	public void performChoiceByEmployeeID(int choice) {
 	Scanner scanner=new Scanner(System.in);
 		int empId=0;
+		int bookId=0;
 		switch (choice) {
 		case 1:
-			System.out.println("enter type of the book to search");
-			String bookType=scanner.nextLine();
-			List<Book> bookList=libraryService.searchByType(bookType);
-			System.out.println("List of Books");
-			bookList.stream().forEach(System.out::println);
+			System.out.println("Employee Details");
+		 empId=scanner.nextInt();
+		//	List<Employee> employeeList=libraryService.
+		//	employeeList.stream().forEach(System.out::println);
+			
 			break;
+		case 2:
+			System.out.println("Enter type of the book to search");
+			String bookType=scanner.nextLine();
+			List<Book> bookTypeList=libraryService.searchByType(bookType);
+			System.out.println("List of Books");
+			bookTypeList.stream().forEach(System.out::println);
+			break;
+		case 3:
+			System.out.println("Enter Name of the book to search");
+			String bookName=scanner.nextLine();
+			List<Book> bookNameList=libraryService.searchByName(bookName);
+			System.out.println("List of Books");
+			bookNameList.stream().forEach(System.out::println);
+			break;
+		case 4:
+			System.out.println("Enter Keyword of the book to search");
+			String bookKeyword=scanner.nextLine();
+			List<Book> bookKeywordList=libraryService.searchByKeyword(bookKeyword);
+			System.out.println("List of Books");
+			bookKeywordList.stream().forEach(System.out::println);
+			break;
+		case 5:
+			System.out.println("Enter Author Name of the book to search");
+			String bookAuthorName=scanner.nextLine();
+			List<Book> bookAuthorNameList=libraryService.searchByAuthorName(bookAuthorName);
+			System.out.println("List of Books");
+			bookAuthorNameList.stream().forEach(System.out::println);
+			break;
+		case 6:
+			System.out.println("My Details");
+			empId=scanner.nextInt();
+		//	List<Employee> employeeDetails=libraryService.searchMyDetails(empId);
+			//employeeDetails.stream().forEach(System.out::println);
+			break;
+		case 7:
+			System.out.println("Issuing Book");
+			 bookId=scanner.nextInt();
+			// List<Book> issueBookList=libraryService.searchByAuthorName(bookAuthorName);
+		//	 issueBookList.stream().forEach(System.out::println);
+			break;
+		case 8:
+			System.out.println("Returning Book");
+			 bookId=scanner.nextInt();
+				// List<Book> returnBookList=libraryService.searchByAuthorName(bookAuthorName);
+			//	returnBookList.stream().forEach(System.out::println);
+			break;
+			default:
+				System.out.println("Invalid Input Details");
 
 	}
 
